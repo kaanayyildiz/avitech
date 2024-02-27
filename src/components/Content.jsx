666f3f27ff0f610ad0680524d519557b5620a6d2
Kaan Ayyıldız <ykaanayyildiz@gmail.com>
2024-02-27 12:38:28 +0300

    updates

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -66,7 +66,9 @@ export default function Content() {
         <div className="grid md:grid-cols-3 p-5">
           <div className="text-2xl md:text-3xl flex items-center gap-2 pb-5 md:p-0">
             <HiMapPin />
-            <a href="https://www.google.com/maps?ll=41.09325,29.09603&z=15&t=m&hl=en&gl=US&mapclient=embed&q=41%C2%B005%2735.7%22N+29%C2%B005%2745.7%22E+41.093250,+29.096030@41.09325,29.09603">Istanbul, Turkey</a>
+            <a href="https://www.google.com/maps?ll=41.09325,29.09603&z=15&t=m&hl=en&gl=US&mapclient=embed&q=41%C2%B005%2735.7%22N+29%C2%B005%2745.7%22E+41.093250,+29.096030@41.09325,29.09603">
+              Istanbul, Turkey
+            </a>
           </div>
           <div className="text-2xl md:text-3xl flex items-center gap-2 pb-5 md:p-0">
             <HiMiniPhone />
@@ -77,43 +79,83 @@ export default function Content() {
             <a href="mailto:info@avitechmetal.com">info@avitechmetal.com</a>
           </div>
         </div>
-        <div className="google-map p-5">
-        <form action="https://fabform.io/f/{form-id}" method="post">
+        <div className="p-5">
+          <form action="#" method="post">
             <div class="mb-5">
-                <label for="name" class="mb-3 block text-base font-medium text-black">
-                    Full Name
-                </label>
-                <input type="text" name="name" id="name" placeholder="Full Name" autoComplete="off"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500" />
+              <label
+                for="name"
+                class="mb-3 block text-base font-medium text-black"
+              >
+                Full Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Full Name"
+                autoComplete="off"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500"
+              />
             </div>
             <div class="mb-5">
-                <label for="email" class="mb-3 block text-base font-medium text-black">
-                    Email Address
-                </label>
-                <input type="email" name="email" id="email" placeholder="example@domain.com" autoComplete="off"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500" />
+              <label
+                for="email"
+                class="mb-3 block text-base font-medium text-black"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="example@domain.com"
+                autoComplete="off"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500"
+              />
             </div>
             <div class="mb-5">
-                <label for="subject" class="mb-3 block text-base font-medium text-black">
-                    Subject
-                </label>
-                <input type="text" name="subject" id="subject" placeholder="Enter your subject" autoComplete="off"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500" />
+              <label
+                for="subject"
+                class="mb-3 block text-base font-medium text-black"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                placeholder="Enter your subject"
+                autoComplete="off"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500"
+              />
             </div>
             <div class="mb-5">
-                <label for="message" class="mb-3 block text-base font-medium text-black">
-                    Message
-                </label>
-                <textarea rows="4" name="message" id="message" placeholder="Type your message" autoComplete="off"
-                    class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500"></textarea>
+              <label
+                for="message"
+                class="mb-3 block text-base font-medium text-black"
+              >
+                Message
+              </label>
+              <textarea
+                rows="4"
+                name="message"
+                id="message"
+                placeholder="Type your message"
+                autoComplete="off"
+                class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-black focus:shadow-md transition duration-500"
+              ></textarea>
             </div>
             <div>
-                <button
-                    class="hover:shadow-form uppercase py-3 px-8 font-semibold px-5 border border bg-black text-white hover:bg-white hover:text-black text-xs transition duration-500 ease-in-out outline-none">
-                    Submit
-                </button>
+              <button class="hover:shadow-form uppercase py-3 px-8 font-semibold px-5 border border bg-black text-white hover:bg-white hover:text-black text-xs transition duration-500 ease-in-out outline-none">
+                Submit
+              </button>
             </div>
-        </form>
+          </form>
+        </div>
+        
+      </div>
+      <div className="w-full bg-gray-100">
+        <div className="mx-auto 2xl:max-w-[90rem] p-5">
           <Map />
         </div>
       </div>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,36 +1,35 @@
 export default function Map() {
   return (
     <>
-      <div class="mt-16 lg:mt-20">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div class="rounded-lg overflow-hidden">
-            <h2 className="text-2xl font-semibold pb-5">Location</h2>
-            <iframe
-              width="100%"
-              height={150}
-              frameBorder={0}
-              scrolling="no"
-              marginHeight={0}
-              marginWidth={0}
-              src="https://maps.google.com/maps?width=100%25&height=600&hl=en&q=41.09325,%2029.09603+(Avitech%20Metal)&t=&z=15&ie=UTF8&iwloc=B&output=embed"
-            >
-              &lt;a href="https://www.gps.ie/"&gt;gps vehicle tracker&lt;/a&gt;
-            </iframe>
-          </div>
-          <div>
-            <div class="max-w-full mx-auto rounded-lg overflow-hidden">
-              <div class="px-6 py-4">
-                <h3 class="text-lg font-medium text-gray-900">Our Address</h3>
-                <p class="mt-1 text-gray-600">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div class="rounded-lg overflow-hidden">
+          <h2 className="text-2xl font-semibold pb-5">Location</h2>
+          <iframe className="border border-gray-300 rounded-md"
+            width="100%"
+            height={150}
+            frameBorder={0}
+            scrolling="no"
+            marginHeight={0}
+            marginWidth={0}
+            
+            src="https://maps.google.com/maps?width=100%25&height=600&hl=en&q=41.09325,%2029.09603+(Avitech%20Metal)&t=&z=15&ie=UTF8&iwloc=B&output=embed"
+          >
+            &lt;a href="https://www.gps.ie/"&gt;gps vehicle tracker&lt;/a&gt;
+          </iframe>
+        </div>
+        <div>
+          <div class="max-w-full mx-auto rounded-lg overflow-hidden">
+            <div class="px-6 py-4">
+              <h3 class="text-lg font-medium text-gray-900">Our Address</h3>
+              <p class="mt-1 text-gray-600">
                 Rüzgarlıbahçe, K Plaza 34805 Beykoz/İstanbul, Türkiye
-                </p>
-              </div>
-              <div class="border-t border-gray-200 px-6 py-4">
-                <h3 class="text-lg font-medium text-gray-900">Hours</h3>
-                <p class="mt-1 text-gray-600">Monday - Friday: 8am - 6pm</p>
-                <p class="mt-1 text-gray-600">Saturday: Closed</p>
-                <p class="mt-1 text-gray-600">Sunday: Closed</p>
-              </div>
+              </p>
+            </div>
+            <div class="border-t border-gray-200 px-6 py-4">
+              <h3 class="text-lg font-medium text-gray-900">Hours</h3>
+              <p class="mt-1 text-gray-600">Monday - Friday: 8am - 6pm</p>
+              <p class="mt-1 text-gray-600">Saturday: Closed</p>
+              <p class="mt-1 text-gray-600">Sunday: Closed</p>
             </div>
           </div>
         </div>
