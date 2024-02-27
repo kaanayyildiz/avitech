380d05e3bca810399de0bad0352df2a6fa6aa44e
Kaan Ayyıldız <ykaanayyildiz@gmail.com>
2024-02-27 13:58:41 +0300

    updates

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,5 @@
-import heroImage from "../assets/avitech-steel-hero-image.jpg"
-import heroImage2 from "../assets/avitech-business.jpg"
-import heroImage3 from "../assets/avitech-aircraft-parts.jpg"
+import heroImage from "../assets/avitech-aircraft-parts-2.jpg"
+import heroImage2 from "../assets/avitech-aircraft-parts.jpg"
 
 export default function Gallery() {
     return (
@@ -9,12 +8,12 @@ export default function Gallery() {
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
         {/* image - start */}
         <a className="group relative flex h-80 items-end overflow-hidden md:h-80">
-          <img src={heroImage} loading="lazy" alt="Avitech Steel" className="absolute inset-0 h-full w-full object-cover object-center transition duration-700 group-hover:scale-110 rotate-180" />
+          <img src={heroImage} loading="lazy" alt="Avitech Steel" className="inset-0 h-full w-full object-cover object-center transition duration-700 group-hover:scale-110 grayscale" />
         </a>
         {/* image - end */}
         {/* image - start */}
-        <a className="group relative flex h-48 items-end overflow-hidden md:col-span-2 md:h-80 grayscale ">
-          <img src={heroImage2} loading="lazy" alt="Avitech Business Corporate" className="sepia absolute inset-0 h-full w-full object-cover transition duration-700 group-hover:scale-110" />
+        <a className="group relative flex h-48 items-end overflow-hidden md:col-span-2 md:h-80">
+          <img src={heroImage2} loading="lazy" alt="Avitech Business Corporate" className="inset-0 h-full w-full object-cover transition duration-700 group-hover:scale-110" />
         </a>
         {/* image - end */}
       </div>
